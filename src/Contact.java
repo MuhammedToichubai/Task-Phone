7ab2cb0c7ec752336e620570f19bdd71808e9a3c
mukhammedasantegin <muhammedtoichubai@gmail.com>
2023-05-04 22:57:21 +0600

    add task for java 10

--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Contackt {
+
+public class Contact {
+    //    1. Contact деген класс тузунуз, полелери(contactName, phoneNumber).
+    private String contactName;
+    private String phoneNumber;
+
+    public Contact (){
+    }
+
+    public Contact(String contactName, String phoneNumber){
+        this.contactName = contactName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getContactName(){
+        return contactName;
+    }
+
+    public void  setContactName(String contactName){
+        this.contactName = contactName;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getInfo(){
+        return String.format("""
+                Contact name: %s
+                Phone number: %s
+                """, contactName, phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "contactName='" + contactName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
+
